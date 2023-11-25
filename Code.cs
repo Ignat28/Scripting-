373fdb09622bfa9343721e154cbe0705dd8090ea
Ignat28 <152006206+Ignat28@users.noreply.github.com>
2023-11-25 17:08:09 +0500

    Add files via upload

---
+++ Code.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Code : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // привет
+        print("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
